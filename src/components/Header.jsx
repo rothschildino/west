0ea8b8cd5d2f69c1fd52f16363c32f702d3c25b0
rothschildino <hiltonteam.clan.su@gmail.com>
2023-04-12 19:10:17 +0300

    1.0.1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Search from './UI/Inputs/Search'
 import '../styles/Header.css'
 import '../App.css'
 import { ButtonMain } from './UI/Buttons/ButtonMain'
-import { AiOutlineMenu, AiOutlineUser } from 'react-icons/ai';
+import { AiOutlineMenu, AiOutlineUser, AiFillShopping } from 'react-icons/ai';
 
 
 
@@ -17,9 +17,15 @@ const Header = () => {
           <div className='logo'>
             <h1>WEST</h1>
           </div>
-        <ButtonMain text={'Меню'} icon={ <AiOutlineMenu /> }/>
-        <Search placeholder={'Я шукаю ...'} />
-        <ButtonMain  text={'Увійти'} icon={ <AiOutlineUser /> }/>
+
+          <div className='fixed_header'>
+            <ButtonMain text={'Меню'} icon={ <AiOutlineMenu /> }/>
+            <Search placeholder={'Я шукаю ...'} />
+            <div>
+            <AiFillShopping className='shop'/>
+            </div>
+          </div>
+        { /* <ButtonMain  text={'Увійти'} icon={ <AiOutlineUser /> }/> */ }
         </div>
       </div>
     </header>
